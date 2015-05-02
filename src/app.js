699d55d7ef9165cf48726929396190f8a4bad07d
Kris Steigerwald <kris@calltrackingmetrics.com>
2015-05-01 20:37:07 -0400

    - add random number function

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 var Server = (function() {
+
   var data = {
     viruses: ['APT1', 'Botnet', 'SPAM', 'StealCreds'],
     owner: ['Anderson Services', 'TMQ Financial'],
-    functions: ['web server', 'ftp server', 'mail server']
+    servers: ['web', 'ftp', 'mail']
   }
 
-  return {}
+  var infected = {
+    '10.223.75.186': {
+       owner: '',
+       virus:[],
+       servers: ''
+      }
+  };
+
+  var randomNumber = function(up,low) {
+    var max = (up || 10), min = (low || 0);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  return {
+    'randomNumber' : randomNumber
+  }
 }());
--- src/test.js
+++ src/test.js
@@ -1,22 +1,20 @@
 module("Test Data Server", {
-  setup:  function() {
-
-  },
-
-  teardown: function() {
-
-  }
+  setup:  function() {},
+  teardown: function() {}
 });
 
 test("data stream", function() {
 
+  ok(true,true);
 });
 test("function count", function() {
 
+  ok(true,true);
 });
 test("virus count", function() {
-
+  ok(true,true);
 });
-test("virus count", function() {
-
+test("random number", function() {
+  var check = Server.randomNumber(), pass=(check > 0 && check < 10);
+  ok(pass, "random check: " +check);
 });
