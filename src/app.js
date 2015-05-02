302a7c2f095a81d027025a9f502f86465a9fb90a
Kris Steigerwald <kris@calltrackingmetrics.com>
2015-05-01 20:14:15 -0400

    - init project with qunit tests and such

---
+++ src/app.js
@@ -0,0 +1,9 @@
+var Server = (function() {
+  var data = {
+    viruses: ['APT1', 'Botnet', 'SPAM', 'StealCreds'],
+    owner: ['Anderson Services', 'TMQ Financial'],
+    functions: ['web server', 'ftp server', 'mail server']
+  }
+
+  return {}
+}());
---
+++ src/test.js
@@ -0,0 +1,22 @@
+module("Test Data Server", {
+  setup:  function() {
+
+  },
+
+  teardown: function() {
+
+  }
+});
+
+test("data stream", function() {
+
+});
+test("function count", function() {
+
+});
+test("virus count", function() {
+
+});
+test("virus count", function() {
+
+});
